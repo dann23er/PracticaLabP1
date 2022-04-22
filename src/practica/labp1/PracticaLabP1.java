1e522ae9b3ca847b4311216e0f283f52ca9debd3
erickdamaya <erickdamaya@localhost>
2022-04-22 15:15:44 -0600

    El anexo de la clase Alumno

---
+++ src/practica/labp1/PracticaLabP1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica.labp1;
+
+/**
+ *
+ * @author erickdamaya
+ */
+public class PracticaLabP1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+       /*
+        Caracteres Especiales
+        \n = Salto de línea
+        */
+        System.out.println("Hola Mundo!" );
+        System.out.println("- Valor Númerico: "+(10+12.34)+"$ \nEstado: "+true+" - Habilitado");
+        System.out.print("Datos de alumnos\nCuenta: 20911036\nNombre Alumno: Erick Amaya\nCarrera:Ingeniería Sistemas\n");
+        
+    }
+    
+}
